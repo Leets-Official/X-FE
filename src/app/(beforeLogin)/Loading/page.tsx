8d26b8e5d3ed077a3860aedefa114e21d2f38e32
Jiwon Kim <jiwonkim0531@gachon.ac.kr>
2024-10-07 18:19:40 +0900

    feat: 구글 소셜 로그인 구현

---
+++ src/app/(beforeLogin)/Loading/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
+export default function Loading() {
+  const router = useRouter();
+
+  //이미 가입한 유저일 시: Home으로 이동
+  const handleHome = () => {
+    router.push("/home");
+  };
+
+  //처음 가입한 유저일 시: 회원가입 모달 페이지로 이동
+  const handleSignup = () => {
+    router.push("/SignupModal");
+  };
+
+  const params = useSearchParams();
+  console.log(params.get("code"));
+  const authCode = params.get("code");
+  console.log("authCode: ", authCode);
+
+  const handleLoginPost = async () => {
+    if (!authCode) {
+      console.error("Authorization code is missing");
+      return;
+    }
+
+    // const encodedCode = encodeURIComponent(authCode);
+    const data = { authCode };
+    console.log("Sending data:", data);
+
+    try {
+      const res = await axios.post(
+        "http://43.203.226.98:8080/api/v1/users/login",
+        data
+      );
+      console.log("res: ", res);
+
+      const accessToken = res.data.data.jwtToken.accessToken;
+      localStorage.setItem("accesstoken", accessToken);
+
+      if (res.data.isExistingMember) {
+        handleHome();
+      } else {
+        handleSignup();
+      }
+    } catch (error) {
+      console.error("Login failed: ", error);
+    }
+  };
+
+  useEffect(() => {
+    if (authCode) {
+      handleLoginPost();
+    } else {
+      console.error("Google authorization code not found.");
+    }
+  }, [authCode]);
+
+  return (
+    <div>
+      <h2>로그인중입니다.</h2>
+    </div>
+  );
+}
---
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+export default function SignupModal() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [image, setImage] = useState("");
+}
