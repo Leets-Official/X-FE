9b4264746ca46c7ec64b359dbfde2237d7cb3d04
Jiwon Kim <jiwonkim0531@gachon.ac.kr>
2024-11-05 13:15:49 +0900

    feat: BackButton UI 구현

--- src/app/(afterLogin)/_component/BackButton.tsx
+++ src/app/(afterLogin)/_component/BackButton.tsx
@@ -1,17 +1,27 @@
 "use client";
+import { useRouter } from "next/navigation";
 import styled from "styled-components";
 
 export default function BackButton() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
-    <StyledButton>
+    <StyledButton onClick={handleBack}>
       <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="3.5859999656677246 4.539999961853027 17.413999557495117 14.920001029968262"
         width={24}
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
+        height={24}
       >
         <g>
-          <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+          <path
+            d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"
+            fill="white"
+          ></path>
         </g>
       </svg>
     </StyledButton>
@@ -21,15 +31,16 @@ export default function BackButton() {
 const StyledButton = styled.button`
   width: 34px;
   height: 34px;
-  background-color: #fff;
+  background-color: #000;
   border: none;
   border-radius: 17px;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  margin-left: 2px;
 
   &:hover {
-    background-color: rgba(15, 20, 25, 0.1);
+    background-color: #333;
   }
 `;
