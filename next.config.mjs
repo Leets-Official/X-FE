472d1cf677d65674a430f7e6c3778e78cd0baabe
김지원 <jiwonkim0531@gachon.ac.kr>
2024-11-10 23:41:00 +0900

    fix: 새로고침시 css 깨짐 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true, // SWC 최적화 활성화
+  compiler: {
+    styledComponents: true, // styled-components 사용 설정
+  },
+  experimental: {
+    forceSwcTransforms: true, // Babel 설정이 있을 경우에도 SWC 사용 강제
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
