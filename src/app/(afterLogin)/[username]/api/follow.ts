12cdb58c7cae1245e8f4bb8c3b0d460cf0388dd9
김지원 <jiwonkim0531@gachon.ac.kr>
2024-11-11 14:34:03 +0900

    feat: unfollow 기능 api 연결 추가

--- src/app/(afterLogin)/[username]/api/follow.ts
+++ src/app/(afterLogin)/[username]/api/follow.ts
@@ -48,3 +48,28 @@ export const followUser = async (
     return { success: false, errorMessage: "네트워크 오류가 발생했습니다." };
   }
 };
+
+export const unfollowUser = async (userId: string, accessToken: string) => {
+  try {
+    const response = await axios.delete<FollowResponse>(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/follows/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    console.log("언팔로우 요청 성공:", response.data.message);
+    return { success: true, message: response.data.message };
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      const errorData = error.response.data as FollowError;
+      return {
+        success: false,
+        errorCode: errorData.code,
+        errorMessage: errorData.message,
+      };
+    }
+    return { success: false, errorMessage: "네트워크 오류가 발생했습니다." };
+  }
+};
